9ecaa9be55636aafaf471f45d5f6b9c34ebc6893
dadu300 <49715386+dadu300@users.noreply.github.com>
2020-06-18 23:54:28 +0200

    Update Hierarchical_Clustering.py

--- Hierarchical_Clustering.py
+++ Hierarchical_Clustering.py
@@ -2,14 +2,11 @@ from matplotlib import pyplot as plt
 from scipy.cluster.hierarchy import dendrogram, linkage
 import numpy as np
 import psycopg2
-import time
-
 
 conn = psycopg2.connect(host="localhost", port=5432, database="GAPT", user="postgres", password="andrew")
 
 cur = conn.cursor()
 
-start_time = time.time()
 # A sample query of all data from the "vendors" table in the "suppliers" database
 cur.execute("""SET search_path = 'NYCPV'; """)
 cur.execute("""SELECT violation_code,issuer_code FROM parking WHERE registration_state = 'NY' LIMIT 10000;""")
@@ -20,35 +17,20 @@ cur.execute("""SET search_path = 'NYCPV'; """)
 cur.execute("""SELECT violation_code,issuer_code FROM parking WHERE registration_state = 'NJ' LIMIT 10000;""")
 B = cur.fetchall()
 print(B)
-print("The time taken to return data from postgreSQL : " + str(float(time.time()) - float(start_time)))
 
-
-
-start_time = time.time()
 concat = np.concatenate((A, B),)
-print("The time taken to concatenate A and B : " + str(float(time.time()) - float(start_time)))
 
-start_time = time.time()
 print (concat.shape)
 plt.scatter(concat[:,0], concat[:,1])
 plt.show()
-print("The time taken to scatter plot the data: " + str(float(time.time()) - float(start_time)))
 
-
-start_time = time.time()
 final_data = linkage(concat, 'single')
-print("The time taken to perform the linkage function : " + str(float(time.time()) - float(start_time)))
 
-
-
-start_time = time.time()
-# calculate full dendrogram
 plt.figure(figsize=(20, 5))
 plt.title('Dendrogram')
 plt.ylabel('distance')
 dendrogram(final_data, truncate_mode='lastp')
 plt.show()
-print("The time taken to plot the dendrogram : " + str(float(time.time()) - float(start_time)))
 
 cur.close()
 conn.close()
